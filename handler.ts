570e68643b8ec9de5f9cbe6df9d9ddd5694d31ba
Khaled Osman <khaled.osmaan@gmail.com>
2019-04-05 13:24:52 +0200

    update and add compression + waitsForEmptyEventLoop = false

--- handler.ts
+++ handler.ts
@@ -2,10 +2,7 @@ import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
 import { lambdaResponse } from './helpers/lambda-response'
 
 export const handle: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  const key = event.headers['x-api-key']
-  // if (!key) {
-  //   return cb('Access denied, please set the x-api-key header')
-  // }
+  context.callbackWaitsForEmptyEventLoop = false
 
   cb(null, lambdaResponse({ message: 'hello there' }, 200))
 
